32988d07ecc8983983187f2490cda83a74cf19f6
Sergio Nova <snovap@unal.edu.co>
2024-09-23 16:20:43 -0500

    modificación del index

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,12 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+/*============ ADD BLUR TO HEADER ================== */
+
+function blurHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
