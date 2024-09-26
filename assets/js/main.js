5f6df92abe99cd7c8a93bf13edf56ded084a62a3
Sergio Nova <snovap@unal.edu.co>
2024-09-25 23:04:02 -0500

    modificación en el css, js y el html

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,46 @@ function blurHeader(){
                         : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
+
+/*============ Mostrar SCROLL UP ================== */
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // Cuando el scroll sea superior a 200 de altura de la ventana gráfica, añada la clase show-scroll a la etiqueta a con la clase scroll-top
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+    // reset: true // Animations repeat 
+})
+
+sr.reveal(`.inicio__data, explore__data, .footer__user, .footer__container`)
+sr.reveal(`.inicio__card`, {delay : 600, distance: '100px', interval : 100})
+sr.reveal(`.nosotros__data, .join__image`, {origin: 'right'})
+sr.reveal(`.nosotros__image , .join__data`, {origin: 'left'})
+sr.reveal(`.atractivos__card`, {interval : 200})
